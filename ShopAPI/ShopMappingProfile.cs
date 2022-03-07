60116c3f267dc47d982597eae755c5a35fa92657
Michał Kwaśniewski <notkwasniewski@gmail.com>
2022-03-07 04:43:17 +0100

    Added Mapping profile for Shop and Product

---
+++ ShopAPI/ShopMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ShopAPI.Entities;
+using ShopAPI.Models;
+
+namespace ShopAPI
+{
+    public class ShopMappingProfile : Profile
+    {
+        private readonly IMapper _mapper;
+
+        public ShopMappingProfile(IMapper mapper)
+        {
+            _mapper = mapper;
+
+            CreateMap<Shop, ShopDto>()
+                .ForMember(s => s.City, c => c.MapFrom(s => s.Address.City))
+                .ForMember(s => s.Street, c => c.MapFrom(s => s.Address.Street))
+                .ForMember(s => s.PostalCode, c => c.MapFrom(s => s.Address.PostalCode));
+
+            CreateMap<Product, ProductDto>();
+        }
+        
+       
+        
+    }
+}
