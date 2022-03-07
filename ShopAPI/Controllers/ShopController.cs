72326252819e2b66dde01aa04da023348516577e
Michał Kwaśniewski <notkwasniewski@gmail.com>
2022-03-07 03:53:31 +0100

    Added GetAll method

--- ShopAPI/Controllers/ShopController.cs
+++ ShopAPI/Controllers/ShopController.cs
@@ -1,6 +1,28 @@
-﻿namespace ShopAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopAPI.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopAPI.Controllers
 {
-    public class ShopController
+    [Route("api/shop")]
+    public class ShopController : ControllerBase
     {
+        private readonly ShopDbContext _dbContext;
+
+        public ShopController(ShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Shop>> GetAll()
+        {
+            var shops = _dbContext.Shops.ToList();
+
+            return Ok(shops);
+        }
+        
+        
     }
 }
