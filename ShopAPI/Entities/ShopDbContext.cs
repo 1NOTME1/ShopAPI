ddc665f28cfddea825898272d7eb4d6c522a4467
Michał Kwaśniewski <notkwasniewski@gmail.com>
2022-03-07 02:09:28 +0100

    - Added 3 Tables from 3 Classes. - Added new rule to columns - Created connection to DB

--- ShopAPI/Entities/ShopDbContext.cs
+++ ShopAPI/Entities/ShopDbContext.cs
@@ -1,6 +1,36 @@
-﻿namespace ShopAPI.Entities
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+
+namespace ShopAPI.Entities
 {
-    public class ShopDbContext
+    public class ShopDbContext : DbContext
     {
+        private string _connectionString = "Server=NOTME;Database=ShopDb;Trusted_Connection=True;";
+
+        public DbSet<Shop> Shops { get; set; } //tworzenie tabel
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //konfiguracja lub dodatkowe właściwości do kolumn w bazie danych
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Shop>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+
+        //funkcja OnConfiguring() pozwala na konfiguracje polaczenia do bazy danych i sprecyzowanie jakiego typu bazy chce uzyc
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString); //połączenie do bazy
+        }
     }
 }
