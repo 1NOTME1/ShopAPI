1c2b23867e0413e57798ea626193f454885cbe8e
Michał Kwaśniewski <notkwasniewski@gmail.com>
2022-03-09 03:03:49 +0100

    Transfered GetById's function logic to new class

---
+++ ShopAPI/Services/ShopService.cs
@@ -0,0 +1,25 @@
+﻿using ShopAPI.Models;
+
+namespace ShopAPI.Services
+{
+    public class ShopService
+    {
+       
+        public ShopDto GetById(int id)
+        {
+            var shop = _dbContext
+                .Shops
+                .Include(s => s.Address)
+                .Include(s => s.Product)
+                .FirstOrDefault(s => s.Id == id);
+
+            if (shop is null) return null;
+
+            var shopDto = _mapper.Map<ShopDto>(shop);
+
+            return shopDto;    
+        }
+
+
+    }
+}
